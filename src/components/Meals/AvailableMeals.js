1f7407ee6f829f8ba66fc8593ec1895389ab523f
HERY SUHERY <herysuhery94@gmail.com>
2023-01-06 13:17:40 +0700

    tambah makanan

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -27,6 +27,12 @@ const DUMMY_MEALS = [
     description: 'Healthy...and green...',
     price: 18.99,
   },
+  {
+    id: 'm5',
+    name: 'Soto',
+    description: 'Delicious...',
+    price: 22.99,
+  },
 ];
 
 const AvailableMeals = () => {
